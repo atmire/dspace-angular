be8087ed13ade8db232b4eeba3b3735502bf4c52
Ali Jaber <ali.jaber@atmire.com>
2025-02-20 21:21:13 -0500

    126857: fix bitstream download page redirect request copy page

--- src/app/bitstream-page/bitstream-download-page/bitstream-download-page.component.ts
+++ src/app/bitstream-page/bitstream-download-page/bitstream-download-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { filter, map, switchMap, take } from 'rxjs/operators';
 import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
-import { hasValue, isNotEmpty } from '../../shared/empty.util';
+import { hasValue, isNotEmpty} from '../../shared/empty.util';
 import { getRemoteDataPayload } from '../../core/shared/operators';
 import { Bitstream } from '../../core/shared/bitstream.model';
 import { AuthorizationDataService } from '../../core/data/feature-authorization/authorization-data.service';
@@ -10,7 +10,7 @@ import { AuthService } from '../../core/auth/auth.service';
 import { combineLatest as observableCombineLatest, Observable, of as observableOf } from 'rxjs';
 import { FileService } from '../../core/shared/file.service';
 import { HardRedirectService } from '../../core/services/hard-redirect.service';
-import { getForbiddenRoute } from '../../app-routing-paths';
+import {getBitstreamRequestACopyRoute, getForbiddenRoute} from '../../app-routing-paths';
 import { RemoteData } from '../../core/data/remote-data';
 import { redirectOn4xx } from '../../core/shared/authorized.operators';
 import { isPlatformServer, Location } from '@angular/common';
@@ -18,6 +18,7 @@ import { DSONameService } from '../../core/breadcrumbs/dso-name.service';
 import { SignpostingDataService } from '../../core/data/signposting-data.service';
 import { ServerResponseService } from '../../core/services/server-response.service';
 import { SignpostingLink } from '../../core/data/signposting-links.model';
+import {BundleDataService} from '../../core/data/bundle-data.service';
 
 @Component({
   selector: 'ds-bitstream-download-page',
@@ -42,6 +43,7 @@ export class BitstreamDownloadPageComponent implements OnInit {
     public dsoNameService: DSONameService,
     private signpostingDataService: SignpostingDataService,
     private responseService: ServerResponseService,
+    private bundleService: BundleDataService,
     @Inject(PLATFORM_ID) protected platformId: string
   ) {
     this.initPageLinks();
@@ -71,7 +73,8 @@ export class BitstreamDownloadPageComponent implements OnInit {
   ngOnInit(): void {
 
     this.bitstreamRD$ = this.route.data.pipe(
-      map((data) => data.bitstream));
+      map((data) => data.bitstream)
+    );
 
     this.bitstream$ = this.bitstreamRD$.pipe(
       redirectOn4xx(this.router, this.auth),
@@ -80,34 +83,76 @@ export class BitstreamDownloadPageComponent implements OnInit {
 
     this.bitstream$.pipe(
       switchMap((bitstream: Bitstream) => {
-        const isAuthorized$ = this.authorizationService.isAuthorized(FeatureID.CanDownload, isNotEmpty(bitstream) ? bitstream.self : undefined);
+        if (!bitstream) {
+          return observableOf([false, false, false, bitstream, ''] as const);
+        }
+        const canDownload$ = this.authorizationService.isAuthorized(
+          FeatureID.CanDownload,
+          bitstream.self
+        );
+
+        const canRequestCopy$ = this.authorizationService.isAuthorized(
+          FeatureID.CanRequestACopy,
+          bitstream.self
+        );
         const isLoggedIn$ = this.auth.isAuthenticated();
-        return observableCombineLatest([isAuthorized$, isLoggedIn$, observableOf(bitstream)]);
+
+        return observableCombineLatest([
+          canDownload$,
+          canRequestCopy$,
+          isLoggedIn$,
+          observableOf(bitstream)
+        ]);
       }),
-      filter(([isAuthorized, isLoggedIn, bitstream]: [boolean, boolean, Bitstream]) => hasValue(isAuthorized) && hasValue(isLoggedIn)),
+      filter(([canDownload, canRequestCopy, isLoggedIn, bitstream]) => {
+        return hasValue(canDownload) && hasValue(canRequestCopy) && hasValue(isLoggedIn);
+      }),
       take(1),
-      switchMap(([isAuthorized, isLoggedIn, bitstream]: [boolean, boolean, Bitstream]) => {
-        if (isAuthorized && isLoggedIn) {
+      switchMap(([canDownload, canRequestCopy, isLoggedIn, bitstream]) => {
+        if (canDownload && isLoggedIn) {
+          // retrieve the actual download link
           return this.fileService.retrieveFileDownloadLink(bitstream._links.content.href).pipe(
             filter((fileLink) => hasValue(fileLink)),
             take(1),
-            map((fileLink) => {
-              return [isAuthorized, isLoggedIn, bitstream, fileLink];
-            }));
+            map((fileLink) =>
+              [canDownload, canRequestCopy, isLoggedIn, bitstream, fileLink] as const
+            )
+          );
         } else {
-          return [[isAuthorized, isLoggedIn, bitstream, '']];
+          // no need for fileLink if you cannot download
+          return observableOf([canDownload, canRequestCopy, isLoggedIn, bitstream, ''] as const);
         }
       })
-    ).subscribe(([isAuthorized, isLoggedIn, bitstream, fileLink]: [boolean, boolean, Bitstream, string]) => {
-      if (isAuthorized && isLoggedIn && isNotEmpty(fileLink)) {
+    ).subscribe(([canDownload, canRequestCopy, isLoggedIn, bitstream, fileLink]) => {
+
+      if (canDownload && isLoggedIn && isNotEmpty(fileLink)) {
         this.hardRedirectService.redirect(fileLink);
-      } else if (isAuthorized && !isLoggedIn) {
+      } else if (canDownload && !isLoggedIn) {
         this.hardRedirectService.redirect(bitstream._links.content.href);
-      } else if (!isAuthorized && isLoggedIn) {
-        this.router.navigateByUrl(getForbiddenRoute(), {skipLocationChange: true});
-      } else if (!isAuthorized && !isLoggedIn) {
+      } else if (!canDownload && canRequestCopy) {
+          this.bundleService.findByHref(bitstream._links.bundle.href).pipe(
+            getRemoteDataPayload(),
+            switchMap(bundle => {
+              if (hasValue(bundle) && hasValue(bundle.item)) {
+                const routeObj = getBitstreamRequestACopyRoute(bundle.item, bitstream);
+                return observableOf(routeObj);
+              } else {
+                return observableOf(null);
+              }
+            }),
+            take(1)
+          ).subscribe(routeObj => {
+            if (hasValue(routeObj)) {
+              this.router.navigate([routeObj.routerLink], { queryParams: routeObj.queryParams });
+            } else {
+              this.router.navigateByUrl(getForbiddenRoute(), { skipLocationChange: true });
+            }
+          });
+      } else if (!canDownload && isLoggedIn && !canRequestCopy) {
+        this.router.navigateByUrl(getForbiddenRoute(), { skipLocationChange: true });
+      } else if (!isLoggedIn) {
         this.auth.setRedirectUrl(this.router.url);
-        this.router.navigateByUrl('login',{replaceUrl: true});
+        this.router.navigateByUrl('login', { replaceUrl: true });
       }
     });
   }
