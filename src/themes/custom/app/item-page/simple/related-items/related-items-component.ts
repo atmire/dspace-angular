d717fa6e072aad73a203f0691393939ec9796ebe
abhinav <abhinav.sidharthan@atmire.com>
2025-04-03 14:19:05 +0200

    Merge remote-tracking branch 'themed/themed-RelatedItemsComponent_contribute-7.6' into themed-RelatedItemsComponent_contribute-main

--- src/themes/custom/app/item-page/simple/related-items/related-items-component.ts
+++ src/themes/custom/app/item-page/simple/related-items/related-items-component.ts
@@ -1,6 +1,15 @@
+import {
+  AsyncPipe,
+  NgClass,
+} from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { RelatedItemsComponent as BaseComponent } from '../../../../../../app/item-page/simple/related-items/related-items-component';
+import { ThemedLoadingComponent } from '../../../../../../app/shared/loading/themed-loading.component';
+import { MetadataFieldWrapperComponent } from '../../../../../../app/shared/metadata-field-wrapper/metadata-field-wrapper.component';
+import { ListableObjectComponentLoaderComponent } from '../../../../../../app/shared/object-collection/shared/listable-object/listable-object-component-loader.component';
+import { VarDirective } from '../../../../../../app/shared/utils/var.directive';
 
 @Component({
   selector: 'ds-themed-related-items',
@@ -8,6 +17,7 @@ import { RelatedItemsComponent as BaseComponent } from '../../../../../../app/it
   // styleUrls: ['./related-items.component.scss'],
   styleUrls: ['../../../../../../app/item-page/simple/related-items/related-items.component.scss'],
   standalone: true,
+  imports: [MetadataFieldWrapperComponent, NgClass, VarDirective, ListableObjectComponentLoaderComponent, ThemedLoadingComponent, AsyncPipe, TranslateModule],
 })
 export class RelatedItemsComponent extends BaseComponent {
 
